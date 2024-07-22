98ddc2d992ebdb9caa62d084e7123b6d91099555
murphyyue <murphyyue@icloud.com>
2024-07-22 23:04:19 +0800

    project initialized

---
+++ README.md
@@ -0,0 +1,4 @@
+# AI4Work
+
+**This is a project to improve your productivity and make your life better**
+**You can use this tool to help you building web page by chatting with AI**
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./containers/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      aspectRatio: {},
+      colors: {},
+    },
+  },
+  plugins: [],
+};
