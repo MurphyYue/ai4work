ebbeecc800dbb1080422d9c9e8333bba83512fe2
murphyyue <murphyyue@icloud.com>
2024-08-02 18:06:01 +0800

    show messages

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ChatInput from './chat/chat-input'
+import { ChatMessages } from './chat/chat-messages'
 
 interface ChatProps {}
 
@@ -7,7 +8,7 @@ const Chat: React.FC<ChatProps> = ({}) => {
   return (
     <div className="flex flex-col h-full p-4">
       <div className="flex-1 overflow-y-auto">
-        
+        <ChatMessages />
       </div>
       <div className="flex">
         <ChatInput />
